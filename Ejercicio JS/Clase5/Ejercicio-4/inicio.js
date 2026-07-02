2e0c3b8b4e11d9766026cde7b2fb0abdf3eb3f7f
Julieta <julieta.insaurralde@gm2dev.com>
2026-07-02 10:28:59 -0300

    Clase5 eje.4

---
+++ Ejercicio JS/Clase5/Ejercicio-4/inicio.js
@@ -0,0 +1,14 @@
+//Parte-A
+const cuadrado =(n) => n * n;
+
+const sumar = (a, b) => a + b;
+
+const inicial=(nombre)=> nombre[0];
+
+//Parte-B
+const mitad = (n) => n / 2; //
+const triple = (n) =>  n * 3; ; // Esta funcion no devuelve nada porque esta mal la sintaxis, para que devuekva algo hay que quitar las llaves.
+
+//parte-C
+const precioConIva = (precio) =>  precio * 1.21 
+console.log(precioConIva(100))
